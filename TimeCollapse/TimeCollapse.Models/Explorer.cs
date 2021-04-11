0907ffcf32324c923782fd8b787f8a9108899dc6
angst-storm <Sergey.Kiprin@urfu.me>
2021-04-11 19:30:30 +0500

    почти реализовал передвижение персонажа
    и сохранение его действий
    осталось по комментарию собрать UpdateLocation
    ну и отрефакторить...

--- TimeCollapse/TimeCollapse.Models/Explorer.cs
+++ TimeCollapse/TimeCollapse.Models/Explorer.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 
 namespace TimeCollapse.Models
 {
-    public class Explorer: MovingObject
+    public class Explorer : MovingObject
     {
+        private readonly Queue<(Action, int)> _actionsSequence = new();
+        private readonly Vector2 _jumpVector;
+        private readonly Vector2 _leftRunVector;
+        private readonly Vector2 _rightRunVector;
         private bool _fromPast;
-        private readonly Queue<(Action, int)> _actionsSequence = new ();
         private (Action, int) _nextAction;
+        public bool Jump = false;
+        public bool LeftRun = false;
+        public bool RightRun = false;
 
-        public Explorer(Point startLocation, Size colliderSize) : base(startLocation, colliderSize)
+        public Explorer(Point startLocation, Size colliderSize, int runSpeed, int jumpHeight) : base(startLocation,
+            colliderSize)
         {
+            _jumpVector = new Vector2(0, -jumpHeight);
+            _leftRunVector = new Vector2(-runSpeed, 0);
+            _rightRunVector = new Vector2(runSpeed, 0);
         }
 
         public void Move(int tick)
         {
             if (!_fromPast)
             {
-                _actionsSequence.Enqueue((UpdateLocation, tick));
+                var jump = Jump && OnFloor ? _jumpVector : Vector2.Zero;
+                var run = (RightRun ? _rightRunVector : Vector2.Zero) + (LeftRun ? _leftRunVector : Vector2.Zero);
+                _actionsSequence.Enqueue((() => UpdateLocation(jump, run), tick));
+                UpdateLocation(run, jump);
             }
             else
             {
--- TimeCollapse/TimeCollapse.Models/MovingObject.cs
+++ TimeCollapse/TimeCollapse.Models/MovingObject.cs
@@ -1,10 +1,15 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
 
 namespace TimeCollapse.Models
 {
     public class MovingObject
     {
+        private static Vector2 _gravity = new(0, 10);
         private readonly Size _colliderSize;
+        private Vector2 _fallVelocity;
         public Point Location;
 
         public MovingObject(Point startLocation, Size colliderSize)
@@ -13,10 +18,52 @@ namespace TimeCollapse.Models
             Location = startLocation;
         }
 
+        public bool OnFloor
+        {
+            get
+            {
+                var collider = Collider;
+                return Game.ActualMap.Blocks.Any(r => TouchesLine(r, new Point(collider.Left, collider.Bottom),
+                    new Point(Collider.Right, collider.Bottom)));
+            }
+        }
+
+        private bool UnderRoof
+        {
+            get
+            {
+                var collider = Collider;
+                return Game.ActualMap.Blocks.Any(r => TouchesLine(r, new Point(collider.Left, collider.Height),
+                    new Point(Collider.Right, collider.Height)));
+            }
+        }
+
         public Rectangle Collider => new(Location, _colliderSize);
 
-        public void UpdateLocation()
+        private bool BesideWall(int x) //x - координата бока коллайдера, который нам нужен
         {
+            var collider = Collider;
+            return Game.ActualMap.Blocks.Any(r =>
+                TouchesLine(r, new Point(x, collider.Height), new Point(x, collider.Bottom)));
+        }
+
+        public void UpdateLocation(Vector2 run, Vector2 jump)
+        {
+            //игрок двигается на run или насколько возможно в сторону run
+            //если jump не равен нулю, то скорость падения игрока равна jump, иначе, если он не на полу, скорость падения игрока равна скорость падения + гравитация
+            //игрок двигается на скорость падения или насколько возможно в сторону скорости падения
+        }
+
+        private bool TouchesLine(Rectangle rect, Point dot1, Point dot2)
+        {
+            return AllLinesPoints(dot1, dot2).Any(rect.Contains);
+        }
+
+        private IEnumerable<Point> AllLinesPoints(Point startPoint, Point endPoint)
+        {
+            var n = new Point(startPoint.Y - endPoint.Y, startPoint.X - endPoint.X);
+            for (var p = startPoint; p != endPoint; p = new Point(p.X + n.X, p.Y + n.Y))
+                yield return p;
         }
     }
 }
