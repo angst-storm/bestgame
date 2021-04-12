79a8686b4299b110d0852c165d41f70efdeddc7c
angst-storm <Sergey.Kiprin@urfu.me>
2021-04-12 15:17:33 +0500

    добавил TimeCollapse.View

---
+++ TimeCollapse/TimeCollapse.View/TestForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace TimeCollapse.View
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
